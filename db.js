698b77e03e27e9f38fb6120c40a5609215fdda1c
Ryokuman <ryokuman@naver.com>
2022-07-04 13:38:13 +0900

    안녕하세요요

---
+++ db.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const mysql = require("mysql");
+const env = process.env.Node_ENV || "development";
+const config = require("./config/config")[env];
+
+const db_info = {
+    host: config.host,
+    user: config.username,
+    password: config.password,
+    port: config.port,
+    database: config.database,
+    multipleStatements: true,
+    dateStirng: "date",
+};
+
+const connection = mysql.createConnection(db_info);
+
+function init() {
+    return mysql.createConnection(db_info);
+}
+
+function connect(conn) {
+    conn.connect((err) => {
+        if (err) console.error(`mysql connection error : ${err}`);
+        else console.log("mysql is connected successfully!");
+    });
+}
+
+module.exports = {
+    init,
+    connect,
+};
