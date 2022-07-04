00079d8a2ef82ee367ca9c2db978823f592c2a64
Ryokuman <ryokuman@naver.com>
2022-07-04 15:28:35 +0900

    idk

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -5,3 +5,14 @@ const passport = require("passport");
 const jwt = require("jsonwebtoken");
 
 db.connect(conn);
+
+const login = async (req, res, next) => {
+    try {
+        passport.authenticate("local", { session: false }, (err, user) => {
+            if (err || !user) {
+                console.log(err);
+                return res.status(400).json({ s });
+            }
+        });
+    } catch (e) {}
+};
